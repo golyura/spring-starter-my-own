a2e3c7df215f08d622ae05de04531d48fb852583
Golubev Yury <gol.y@mail.ru>
2024-02-23 16:12:13 +0300

    lesson-11

---
+++ src/main/java/com/gol/spring/database/entity/Company.java
@@ -0,0 +1,4 @@
+package com.gol.spring.database.entity;
+
+public record Company(Integer id) {
+}
--- src/main/java/com/gol/spring/database/repository/CompanyRepository.java
+++ src/main/java/com/gol/spring/database/repository/CompanyRepository.java
@@ -1,11 +1,33 @@
 package com.gol.spring.database.repository;
 
+import com.gol.spring.bpp.Auditing;
 import com.gol.spring.bpp.InjectBean;
+import com.gol.spring.bpp.Transaction;
+import com.gol.spring.database.entity.Company;
 import com.gol.spring.database.pool.ConnectionPool;
+import jakarta.annotation.PostConstruct;
 
-public class CompanyRepository {
+import java.util.Optional;
+
+@Transaction
+@Auditing
+public class CompanyRepository implements CrudRepository<Integer, Company>{
     @InjectBean
     private ConnectionPool connectionPool;
 
+    @PostConstruct
+    private void init() {
+        System.out.println("init company repository");
+    }
 
+    @Override
+    public Optional<Company> findById(Integer id) {
+        System.out.println("findById method...");
+        return Optional.of(new Company(id));
+    }
+
+    @Override
+    public void delete(Company entity) {
+        System.out.println("delete method...");
+    }
 }
