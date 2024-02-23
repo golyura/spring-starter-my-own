165b1e47e1d1fc5632f4370ab7d2b8fe41b57abb
Golubev Yury <gol.y@mail.ru>
2024-02-23 17:16:04 +0300

    lesson-12

--- src/main/java/com/gol/spring/database/repository/CompanyRepository.java
+++ src/main/java/com/gol/spring/database/repository/CompanyRepository.java
@@ -6,14 +6,31 @@ import com.gol.spring.bpp.Transaction;
 import com.gol.spring.database.entity.Company;
 import com.gol.spring.database.pool.ConnectionPool;
 import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 
+import java.util.List;
 import java.util.Optional;
 
 @Transaction
 @Auditing
-public class CompanyRepository implements CrudRepository<Integer, Company>{
-    @InjectBean
-    private ConnectionPool connectionPool;
+public class CompanyRepository implements CrudRepository<Integer, Company> {
+//    @Autowired
+    //    @Resource(name = "pool1")
+//    @Qualifier("pool1")
+    private ConnectionPool pool1;
+    @Autowired
+    private List<ConnectionPool> pools;
+    @Value("${db.pool.size}")
+    private Integer poolSize;
+
+
+//    @Autowired
+//    @Qualifier("pool1")
+//    public void setPool1(ConnectionPool pool1) {
+//        this.pool1 = pool1;
+//    }
 
     @PostConstruct
     private void init() {
