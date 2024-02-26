8c31bb20c53cbac17dbb5d0088447cd6d923bf9a
Golubev Yury <gol.y@mail.ru>
2024-02-26 12:19:50 +0300

    lesson-18

--- src/main/java/com/gol/spring/config/JpaConfiguration.java
+++ src/main/java/com/gol/spring/config/JpaConfiguration.java
@@ -2,12 +2,22 @@ package com.gol.spring.config;
 
 import com.gol.spring.config.condition.JpaCondition;
 import jakarta.annotation.PostConstruct;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
 
 @Conditional(JpaCondition.class)
 @Configuration
 public class JpaConfiguration {
+
+
+//    @Bean
+//    @ConfigurationProperties(prefix = "db")
+//    public DatabaseProperties databaseProperties() {
+//        return new DatabaseProperties();
+//    }
+
 
     @PostConstruct
     void init() {
