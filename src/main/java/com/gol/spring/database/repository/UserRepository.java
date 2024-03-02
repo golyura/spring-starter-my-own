77ceb768bf9e2e577806b24bef0291e3e4ccdf80
Golubev Yury <gol.y@mail.ru>
2024-03-02 12:06:46 +0300

    lesson-25

--- src/main/java/com/gol/spring/database/repository/UserRepository.java
+++ src/main/java/com/gol/spring/database/repository/UserRepository.java
@@ -1,11 +1,19 @@
 package com.gol.spring.database.repository;
 
+import com.gol.spring.database.entity.Role;
 import com.gol.spring.database.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
@@ -17,5 +25,25 @@ public interface UserRepository extends JpaRepository<User, Long> {
     @Query(value = "SELECT u.* FROM users u WHERE u.username = :username",
             nativeQuery = true)
     List<User> findAllByUsername(String username);
+
+    @Modifying(clearAutomatically = true, flushAutomatically = true)
+    @Query("update User u " +
+            "set u.role = :role " +
+            "where u.id in (:ids)")
+    int updateRole(Role role, Long... ids);
+
+    Optional<User> findTopByOrderByIdDesc();
+
+    List<User> findTop3ByBirthDateBefore(LocalDate birthDate, Sort sort);
+
+//    List<User> findAllBy(Pageable pageable);
+
+    // Collection, Stream
+    // Streamable, Slice, Page
+    // @EntityGraph("User.company")
+    @EntityGraph(attributePaths = {"company", "company.locales"})
+    @Query(value = "select u from User u",
+            countQuery = "select count(distinct u.firstname) from User u")
+    Page<User> findAllBy(Pageable pageable);
 }
 
--- src/test/java/com/gol/spring/integration/database/repository/UserRepositoryTest.java
+++ src/test/java/com/gol/spring/integration/database/repository/UserRepositoryTest.java
@@ -1,11 +1,19 @@
 package com.gol.spring.integration.database.repository;
 
 
+import com.gol.spring.database.entity.Role;
+import com.gol.spring.database.entity.User;
 import com.gol.spring.database.repository.UserRepository;
 import com.gol.spring.integration.annotation.IT;
 import lombok.RequiredArgsConstructor;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+
+import java.time.LocalDate;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
 
 @IT
 @RequiredArgsConstructor
@@ -13,15 +21,62 @@ public class UserRepositoryTest {
     private final UserRepository userRepository;
 
     @Test
+    void checkPageable() {
+        var pageable = PageRequest.of(0, 2, Sort.by("id"));
+        var slice = userRepository.findAllBy(pageable);
+        slice.forEach(user -> System.out.println(user.getCompany().getName()));
+
+        while (slice.hasNext()) {
+            slice = userRepository.findAllBy(slice.nextPageable());
+            slice.forEach(user -> System.out.println(user.getCompany().getName()));
+        }
+    }
+
+    @Test
+    void checkSort() {
+        var sortBy = Sort.sort(User.class);
+        var sort = sortBy.by(User::getFirstname)
+                .and(sortBy.by(User::getLastname));
+
+        var sortById = Sort.by("firstname").and(Sort.by("lastname"));
+        var allUsers = userRepository.findTop3ByBirthDateBefore(LocalDate.now(), sort);
+        assertThat(allUsers).hasSize(3);
+    }
+
+    @Test
+    void checkFirstTop() {
+        var topUser = userRepository.findTopByOrderByIdDesc();
+        assertTrue(topUser.isPresent());
+        topUser.ifPresent(user -> assertEquals(5L, user.getId()));
+    }
+
+
+    @Test
+    void checkUpdate() {
+        var ivan = userRepository.getReferenceById(1L);
+        assertSame(Role.ADMIN, ivan.getRole());
+        ivan.setBirthDate(LocalDate.now());
+
+        var resultCount = userRepository.updateRole(Role.USER, 1L, 5L);
+        assertEquals(2, resultCount);
+
+//        ivan.getCompany().getName();
+
+        var theSameIvan = userRepository.getReferenceById(1L);
+        assertSame(Role.USER, theSameIvan.getRole());
+    }
+
+
+    @Test
     void checkQueries() {
         var users = userRepository.findAllBy("a", "ov");
-        Assertions.assertThat(users).hasSize(3);
+        assertThat(users).hasSize(3);
     }
 
     @Test
     void checkNativeQueries() {
         var users = userRepository.findAllByUsername("ivan@gmail.com");
-        Assertions.assertThat(users).hasSize(1);
+        assertThat(users).hasSize(1);
     }
 
 }
