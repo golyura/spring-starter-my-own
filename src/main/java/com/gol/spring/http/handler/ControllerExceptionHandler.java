cf47a57ee956db2eadf16f305bae7fd74001b423
Golubev Yury <gol.y@mail.ru>
2024-03-11 10:43:20 +0300

    lesson - 43 validation controllerAdvice

---
+++ src/main/java/com/gol/spring/http/handler/ControllerExceptionHandler.java
@@ -0,0 +1,19 @@
+package com.gol.spring.http.handler;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+
+
+@Slf4j
+@ControllerAdvice
+public class ControllerExceptionHandler /*extends ResponseEntityExceptionHandler*/ {
+
+    @ExceptionHandler(Exception.class)
+    public String handleExceptions(Exception exception, HttpServletRequest request) {
+        log.error("Failed to return response", exception);
+        return "error/error500";
+    }
+}
