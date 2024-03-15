d58c09d0d4b6c9346362fc075379917455e5e4bf
Golubev Yury <gol.y@mail.ru>
2024-03-15 10:19:59 +0300

    lesson - 46 security dao

--- src/main/java/com/gol/spring/database/entity/User.java
+++ src/main/java/com/gol/spring/database/entity/User.java
@@ -32,11 +32,14 @@ public class User extends AuditingEntity<Long> {
     @Column(unique = true, nullable = false)
     private String username;
 
+    private String password;
+
     private LocalDate birthDate;
 
     private String firstname;
 
     private String lastname;
+
 
     private String image;
 
--- src/main/java/com/gol/spring/database/repository/UserRepository.java
+++ src/main/java/com/gol/spring/database/repository/UserRepository.java
@@ -67,5 +67,8 @@ public interface UserRepository extends
             "WHERE company_id = :companyId",
             nativeQuery = true)
     List<PersonalInfo2> findAllByCompanyId(Integer companyId);
+
+    Optional<User> findByUsername(String username);
 }
+
 
