7d0246e73334e9a0318dcc4ae37f4fdd03d616e2
Golubev Yury <gol.y@mail.ru>
2024-03-16 18:09:44 +0300

    lesson - 50 security ant matchers

--- src/main/java/com/gol/spring/config/SecurityConfiguration.java
+++ src/main/java/com/gol/spring/config/SecurityConfiguration.java
@@ -2,14 +2,14 @@ package com.gol.spring.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+import static com.gol.spring.database.entity.Role.ADMIN;
+import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
 
 @Configuration
 //@EnableWebSecurity
@@ -19,7 +19,13 @@ public class SecurityConfiguration {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests((requests) -> requests.anyRequest().authenticated())
+                .authorizeHttpRequests(urlConfig -> urlConfig
+                        .requestMatchers(antMatcher("/login/**"), antMatcher("/users/registration/**"), antMatcher("/v3/api-docs/**"), antMatcher("/swagger-ui/**")).permitAll()
+                        .requestMatchers(antMatcher("/users")).permitAll()
+                        .requestMatchers(antMatcher("/users/{\\d+}/delete")).hasAuthority(ADMIN.getAuthority())
+                        .requestMatchers(antMatcher("/admin/**")).hasAuthority(ADMIN.getAuthority())
+                        .anyRequest().authenticated()
+                )
 //                .httpBasic(Customizer.withDefaults());
                 .logout(logout -> logout
                         .logoutUrl("/logout")
